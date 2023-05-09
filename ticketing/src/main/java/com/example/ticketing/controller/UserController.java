ed09f099762559aba6480387b3fa3e26990c7c68
trajkovskakarolina <trajkovskakarolina@gmail.com>
2023-05-10 01:40:12 +0200

    user model, repository; database connection

---
+++ ticketing/src/main/java/com/example/ticketing/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.ticketing.controller;
+
+import com.example.ticketing.model.User;
+import com.example.ticketing.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+}
---
+++ ticketing/src/main/java/com/example/ticketing/model/User.java
@@ -0,0 +1,56 @@
+package com.example.ticketing.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String name;
+    private String email;
+
+    public String getId() {
+        return null;
+    }
+
+    // getter, setters, contructors
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return name;
+    }
+
+    public void setUsername(String username) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
