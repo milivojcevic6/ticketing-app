9fa58a524253fd62f1bf400d4355ee32a712a9ef
trajkovskakarolina <trajkovskakarolina@gmail.com>
2023-05-11 16:08:12 +0200

    controller update

--- ticketing/src/main/java/com/example/ticketing/model/User.java
+++ ticketing/src/main/java/com/example/ticketing/model/User.java
@@ -19,15 +19,24 @@ public class User {
     private String name;
     private String email;
 
-    public String getId() {
-        return null;
-    }
 
     // getter, setters, contructors
 
 
+    public Long getId() {
+        return id;
+    }
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getName() {
@@ -35,14 +44,6 @@ public class User {
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return name;
-    }
-
-    public void setUsername(String username) {
         this.name = name;
     }
 
