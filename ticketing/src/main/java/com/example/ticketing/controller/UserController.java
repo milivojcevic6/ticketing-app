0081a0e4bb66ffb9ea4c547b9c7d7e574f22b338
trajkovskakarolina <trajkovskakarolina@gmail.com>
2023-05-11 16:00:41 +0200

    postman connection works

--- ticketing/src/main/java/com/example/ticketing/controller/UserController.java
+++ ticketing/src/main/java/com/example/ticketing/controller/UserController.java
@@ -2,15 +2,25 @@ package com.example.ticketing.controller;
 
 import com.example.ticketing.model.User;
 import com.example.ticketing.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+
 
 }
