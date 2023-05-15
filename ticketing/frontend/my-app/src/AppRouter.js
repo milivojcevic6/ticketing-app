6184e94acde293e2a4d5bc626e0ee12e1e9c78a1
Milan <998miki@gmail.com>
2023-05-15 05:27:41 +0200

    Login and Register view added

--- ticketing/frontend/my-app/src/AppRouter.js
+++ ticketing/frontend/my-app/src/AppRouter.js
@@ -4,12 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Home";
 import Profile from "./components/Profile";
 import Tickets from "./components/Tickets";
+import Login from "./components/Login";
+import {LogIn} from "react-feather";
 
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage/>} />
+                <Route path="login" element={<Login/>} />
                 <Route path="profile" element={<Profile/>} />
                 <Route path="tickets" element={<Tickets/>} />
             </Routes>
---
+++ ticketing/frontend/my-app/src/components/Login/index.js
@@ -0,0 +1,51 @@
+﻿import React, { useState } from 'react';
+import './login.css';
+import loginImage from '../../logo.png';
+
+function Login() {
+    const [isLogin, setIsLogin] = useState(true);
+
+    const handleTabChange = () => {
+        setIsLogin(!isLogin);
+    };
+
+    return (
+        <div className="login-container">
+            <div className="image-container">
+                <img src={loginImage} alt="Logo" className="logo" />
+            </div>
+            <div className="form-container">
+                <div className="tabs">
+                    <button
+                        className={`tab-button ${isLogin ? 'active' : ''}`}
+                        onClick={handleTabChange}
+                    >
+                        Login
+                    </button>
+                    <button
+                        className={`tab-button ${isLogin ? '' : 'active'}`}
+                        onClick={handleTabChange}
+                    >
+                        Register
+                    </button>
+                </div>
+                {isLogin ? (
+                    <form className="login-form">
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <button type="submit">Login</button>
+                    </form>
+                ) : (
+                    <form className="register-form">
+                        <input type="text" placeholder="Username" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button type="submit">Register</button>
+                    </form>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Login;
