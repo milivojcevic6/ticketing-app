80064f02de344a4fb4ca724760590aff459647c9
Milan <998miki@gmail.com>
2024-05-25 17:20:42 +0200

    changed basedUrl to 8000

--- ticketing/frontend/my-app/src/api/axios.js
+++ ticketing/frontend/my-app/src/api/axios.js
@@ -1,5 +1,6 @@
 ﻿import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8080'
+    // baseURL: 'http://localhost:8080'
+    baseURL: 'http://localhost:8000'
 });
