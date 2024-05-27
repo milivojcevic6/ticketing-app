697e886fbce3a380872f678453e3ccaa478a080d
Dimitar <dimitar.pelivanov@gmail.com>
2024-05-27 11:41:40 +0200

    unique card code id

--- ticketing/djangoApi/card/models.py
+++ ticketing/djangoApi/card/models.py
@@ -7,6 +7,6 @@ from django.db import models
 
 class Card(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    code = models.CharField(max_length=255)
+    code = models.CharField(max_length=255, unique=True)
     expiration_date = models.DateField()
     activation_date = models.DateField()
